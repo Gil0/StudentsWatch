014bf59cd23439137904593466d7332237299482
Gil0 <gilrafael47@gmail.com>
2017-09-22 15:41:48 -0500

    Modificacion mensajes de validacion

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Esta información NO corresponde a la de nuestros registros.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'password' => 'La Contraseña debe tener al menos 6 caracteres y coincidir con la confirmación.',
+    'reset' => 'Tu Contraseña ha sido restablecida!',
+    'sent' => 'Te hemos enviado un link de restablecimiento de Contraseña a tu correo electrónico.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "No existe un usuario con ese correo electrónico en nuestros registros.",
 
 ];
