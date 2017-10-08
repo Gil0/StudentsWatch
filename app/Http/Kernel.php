96e110d7053c09a5d371cd71f270dcd98fd34247
Gil0 <gilrafael47@gmail.com>
2017-10-07 22:25:50 -0500

    Se agrego la clase profesor

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,5 +49,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'administrador' => \App\Http\Middleware\administrador::class,
+        'profesor' => \App\Http\Middleware\profesor::class
     ];
 }
