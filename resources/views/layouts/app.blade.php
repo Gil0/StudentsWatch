844e9d52317dc924d0237b78270f207451a7ff67
nancytap <nancylykens@gmail.com>
2017-11-07 11:06:03 -0600

    cambio de color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
    <!--ESTILOS MIOS-->
    <link rel="stylesheet" href="../assets/css/estilos.css">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ elixir('../assets/css/estilos.css') }}" rel="stylesheet"> --}}
     <style>
         body {
             font-family: 'Lato';
