976ffb79d5a0170556b9595d308ab0066925d2ae
nancytap <nancylykens@gmail.com>
2017-11-07 10:18:20 -0600

    creacion de pagina de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,29 @@
 @if(Auth::guest())
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading">Bienvenido</div>
-                <div class="panel-body">
-                    Hola Usuario no registrado.
+        <div class="panel panel-default posicion">
+            <center><div class="panel-heading intro">Bienvenido...</div></center>
+                <div class="panel-body ">
+                    <center>Hola Usuario no registrado.</center>
                 </div>
             </div>
         </div>
     </div>
-</div>      
+</div>  
+<!------ Slider ------------>
+<div class="slider">
+	      	<div class="slider-wrapper theme-default">
+	            <div id="slider" class="nivoSlider">
+	                <img src="../assets/img/bueno.png" alt="" />
+	                <img src="images/banner3.jpg" alt="" />
+	                <img src="images/banner2.jpg" alt="" />
+	                <img src="images/banner1.jpg" alt="" />
+	                <img src="images/banner5.jpg" alt="" />
+	            </div>
+	       </div>
+         </div>
+         
+  <!------End Slider ------------>    
 @else
     @if (Auth::user()->is_admin == true)
         @if (Auth::user()->is_profesor == true)                                                        
@@ -30,10 +44,10 @@
                     <li><a href="{{ url('/login') }}">Comentarios</a></li>
                  </ul>
             </div>
-            <div class="row">
+            <div class="row intro">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Bienvenido</div>
+                        <div class="panel-heading intro">Bienvenido</div>
                             <div class="panel-body">
                                 Hola [Administrador][Profesor][Tutor].
                             </div>
@@ -73,12 +87,12 @@
                     <li><a href="{{ url('/Admin/Comentarios') }}">Comentarios</a></li>
                  </ul>
             </div>
-            <div class="row">
+            <div class="row intro">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Bienvenido</div>
+                       <center> <div class="panel-heading">Bienvenido  ... </div></center>
                             <div class="panel-body">
-                                Hola Administrador.
+                               <center> Hola Administrador.</center>
                             </div>
                         </div>
                     </div>
