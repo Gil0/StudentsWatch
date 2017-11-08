7a5fd9dd1e199d707e2e3515994edc0d75f20a07
Manolo <manolinsk8@hotmail.com>
2017-11-08 12:53:50 -0600

    creando vista de alumnos (administrador)

---
+++ resources/views/Admin/AdminUsuarios.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+@section('content')
+<style>
+    i.fa-trash{
+      color: #d9534f;
+    }
+    i.fa-trash:hover{
+        color:red;
+    }
+</style>
+<meta name="csrf_token" content="{{ csrf_token() }}" /> <!--Se necestia este metadato para poder hacer AJAX, se envia el csrf_token al server para validar que si existe la sesion -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<style>
+</style>
+<div class="container">
+<div class="navar">
+<ul class="nav nav-pills"> 
+    <li><a href="{{ url('/') }}">Inicio</a></li>
+    <li><a href="{{ url('/Admin/Alumnos') }}">Alumnos</a></li>
+    <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+    <li><a href="{{ url('/Admin/Comentarios') }}">Comentarios</a></li>
+    <li><a href="{{ url('/Admin/Materias') }}">Materias</a></li>
+</ul>
+</div>
+    <div class="row">
+        <div class="col-md-12 ">
+            <div class="panel panel-default">
+                    <div class="panel-body">
+                <center>  <p> Alumnos registrados en el sistema</p> </center>
+
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                        <table class="table">
+                        <thread class="thead-inverse">
+                            <tr>
+                                <th> <div align="center"># </div></th>
+                              <th> <div align="center">Nombre </div> </th>
+                              <th> <div align="center">matricula </div> </th>
+                              <th> <div align="center">email </div> </th>
+                               <th><div align="center">Eliminar</div> </th>
+                            </tr>
+                        </thread>
+                        <tbody>
+                        @foreach($users as $users)
+                            <tr>
+                             <th scope="row"><div align="center"> {{$users->id}}</div></th>
+                             <th><div align="center"> {{$users->name}}</div> </th>
+                             <th><div align="center"> {{$users->matricula}}</div> </th>
+                             <th><div align="center"> {{$users->email}}</div> </th>
+                            <th> <div align="center"> <i class="fa fa-trash fa-2x icondelete" aria-hidden="true" value="{{$users->id}}"></i></div></th>
+                            </tr>
+                         @endforeach
+                        </tbody>
+                    </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
