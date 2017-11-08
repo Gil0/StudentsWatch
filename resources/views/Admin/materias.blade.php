539f1f22e3deead15d1700e0178cdb509997ac7c
Manolo <manolinsk8@hotmail.com>
2017-11-08 02:37:34 -0600

    creando vistas de materias (Administrador y alumno)

---
+++ resources/views/Admin/materias.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+@section('content')
+<!--<meta name="csrf_token" content="{{ csrf_token() }}" /> <!Se necestia este metadato para poder hacer AJAX, se envia el csrf_token al server para validar que si existe la sesion -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<style>
+    @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
+    @import url('https://fonts.googleapis.com/css?family=Anton');
+   
+    .profesor{
+        color: #3385ff;
+        font-family: 'Anton', sans-serif;
+        letter-spacing: 2px;
+        font-size: 50px;
+    }
+
+    i.fa-trash{
+      color: #d9534f;
+    }
+    i.fa-trash:hover{
+        color:red;
+    }
+   
+    i.fa-pencil-square{
+      color: #2eb82e;
+    }
+    i.fa-pencil-square:hover{
+        color:#145214;
+    }
+  
+</style>
+
+    <div class="container">
+      <div class="navar">
+        <ul class="nav nav-pills"> 
+            <li><a href="{{ url('/') }}">Inicio</a></li>
+            <li><a href="{{ url('/login') }}">Alumnos</a></li>
+            <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+            <li><a href="{{ url('/login') }}">Comentarios</a></li>
+            <li><a href="{{ url('/Admin/Materias') }}">Materias</a></li>
+        </ul>
+     </div>
+     <div class="col-sm-12">
+                <div align="center" >
+                    <p class="profesor">Materias</p>
+                </div>
+                <div align="center">
+                <div class="panel-heading">
+
+                       <button class="btn btn-success" id="nuevaMat" style="width:100%;" value="">Agregar Materia</button>
+               </div>
+                </div>
+                <div class="panelesp">
+                    <div class="panel-body">
+                        <table class="table">
+                            <thread class="thead-inverse">
+                                <tr>
+                                    <th> <div align="center"># </div></th>
+                                  <th> <div align="center">Nombre </div> </th>
+                                    <th> <div align="center">Editar</div> </th>
+                                   <th><div align="center">Eliminar</div> </th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                            @foreach($materias as $materia)
+                                <tr>
+                                 <th scope="row"><div align="center"> {{$materia->idMateria}}</div></th>
+                                 <th><div align="center"> {{$materia->nombre}}</div> </th>
+                                  <th> <div align="center"> <i class="fa fa-pencil-square fa-2x iconpencil" aria-hidden="true" value=""></i></div></th>
+                                <th> <div align="center"> <i class="fa fa-trash fa-2x icondelete" aria-hidden="true" value="{{$materia->idMateria}}"></i></div></th>
+                                </tr>
+                             @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="nuevaMateria" tabindex="-1" role="dialog" aria-labelledby="Nueva Materia">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Nueva Materia</h4>
+      </div>
+
+      <form id="formMateria" method="POST">
+
+
+      {{ csrf_field() }} <!-- ESTE TOKEN ES IMPORTANTE PARA PODER ENVIAR DATOS AL SERVER... si no lo incluyes habra error ya que la informacion no es "confiable" -->
+        <div class="modal-body">
+            <input type="text" class="form-control" placeholder="materia" name="materia" required><br>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal" id="cancelar">Cerrar</button>
+            <button type="submit" class="btn btn-primary" id="crear">Guardar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- modal seguridad eliminar materia-->
+<div class="modal fade" id="eliminarMateria" tabindex="-1" role="dialog" aria-labelledby="Eliminar Profesor">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+           <p class="lead" style="text-align:center;">¿Estas seguro de eliminar ésta materia?</p>
+      </div>
+      <div class="modal-footer">
+        <form method="POST" action="" id="eliminarMa">
+            {{ csrf_field() }}
+            <button type="submit" class="btn btn-danger" style="width:100%;">SI</button>
+        </form>
+        <button type="button" class="btn btn-default" style="width:100%;" data-dismiss="modal">NO</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+ 
+
+<script>
+    $(document).ready(function(){               
+        $('i.fa-trash').click(function(){
+           $('#eliminarMateria').modal('show');
+           $('form#eliminarMa').attr('action','/Admin/Materia/'+$(this).attr('value')+'/eliminar');
+         });
+         
+        $('i.fa-pencil-square').click(function(){
+           window.location.href = '/Usuario/Comentarios/'+$(this).attr('value')+'/ver';
+        });
+        $('button#nuevaMat').click(function(){
+            $('#nuevaMateria').modal('show');
+            $('form#formMateria').attr('action', '/Admin/materia/crear/' );
+        });
+
+        
+    });
+</script>
+@endsection
---
+++ resources/views/Usuario/materias.blade.php
@@ -0,0 +1,212 @@
+@extends('layouts.app')
+
+@section('content')
+<!--<meta name="csrf_token" content="{{ csrf_token() }}" /> <!Se necestia este metadato para poder hacer AJAX, se envia el csrf_token al server para validar que si existe la sesion -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<style>
+    @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
+    @import url('https://fonts.googleapis.com/css?family=Anton');
+  
+    .profesor{
+        color: #3385ff;
+        font-family: 'Anton', sans-serif;
+        letter-spacing: 2px;
+        font-size: 50px;
+    }
+
+    }
+</style>
+
+    @if (Auth::user()->is_admin == true)
+        @if (Auth::user()->is_profesor == true)                                                        
+            @if (Auth::user()->is_tutor == true)                
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mis alumnos</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Información</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                 </ul>
+            </div>
+            
+            @else                
+                <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Informacion</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>                                                
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores/') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                  </ul>
+            </div>
+           
+            @endif                                        
+        @else            
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                 </ul>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Bienvenido</div>
+                            <div class="panel-body">
+                                Hola Administrador.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>                                                  
+        @endif                                        
+    @else
+        @if (Auth::user()->is_profesor == true)
+            @if (Auth::user()->is_tutor == true)                
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Alumnos</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Información</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>
+                 </ul>
+            </div>
+                                                       
+            @else            
+            <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/Profesor/Informacion/'.encrypt(Auth::user()->id)) }}">Mi Informacion</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>                                                
+                  </ul>
+            </div>
+                                          
+            @endif
+        @else            
+            <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Mapa Curricular</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/Usuario/Profesores') }}">Ver Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Ver Comentarios</a></li>
+                 </ul>
+            </div>
+                                                             
+        @endif                                        
+    @endif 
+
+            <div class="col-sm-9">
+                <div align="center" >
+                    <p class="profesor">Materias</p>
+                </div>
+                <div class="panelesp">
+                    <div class="panel-body">
+                        <table class="table table-hover">
+                            <thread>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre</th>
+                                    <th id="materiaCursada">Agregar Materia Cursada</th>
+                                    <th>Agregar Materia en curso</th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                            @foreach($materias as $materias)
+                                <tr>
+                                    <th scope="row">{{$materias->idMateria}}</th>
+                                    <th>{{$materias->nombre}} </th>
+        
+                                   
+                                    <th>
+                                        <div class="panel-heading">
+
+                                            <button class="btn btn-success" id="nuevaMateriaCursada" style="width:100%;" value="{{$materias->idMateria}}">Agregar Materia aprobada</button>
+                                        </div>
+                                    </th>
+
+                                    <th>
+                                        <div class="panel-heading">
+
+                                            <button class="btn btn-success" id="materiaEnCurso" style="width:100%;" value="{{$materias->idMateria}}">Agregar materia en curso</button>
+                                        </div>
+                                    </th>
+
+                                   
+                                </tr>
+                             @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="MateriaCursada" tabindex="-1" role="dialog" aria-labelledby="Agregar Materia Cursada">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Agregar Materia Cursada </h4>
+      </div>
+
+      <form id="formMateriaCursada" method="POST">
+
+
+      {{ csrf_field() }} <!-- ESTE TOKEN ES IMPORTANTE PARA PODER ENVIAR DATOS AL SERVER... si no lo incluyes habra error ya que la informacion no es "confiable" -->
+        <div class="modal-body">
+           <p>hola {{Auth::user()->name}} estás seguro de que deseas agregar esta materia a tus materias cursadas </p>
+           
+            <input type="hidden" name="user" value="{{Auth::user()->id}}">
+        </div>
+        <div class="modal-footer">
+        <button type="submit" class="btn btn-primary" id="crearMateriaCursada">Agregar Materia</button>
+            <button type="button" readonly class="btn btn-default" data-dismiss="modal" id="cancelar">Cerrar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>
+    $(document).ready(function(){               
+        $('i.fa-plus-circle').click(function(){
+            window.location.href = '/Usuario/Profesores/'+$(this).attr('value')+'/Ver';
+         } );
+         
+        $('i.fa-pencil-square').click(function(){
+           window.location.href = '/Usuario/Comentarios/'+$(this).attr('value')+'/ver';
+        });
+        $('button#btn-success').click(function(){
+            $('#MateriaCursada').modal('show');
+            $('form#formMateriaCursada').attr('action', '/user/MateriaCursada/crear/'+$(this).attr('value') );
+        });
+
+        $('button#materiaEnCurso').click(function(){
+            $('#MateriaCursada').modal('show');
+            $('form#formMateriaCursada').attr('action', '/user/MateriaCursada/crear/'+$(this).attr('value') );
+        });
+
+        $("button.btn-success").click(function(){
+            $(this).removeClass("btn-success");
+            $(this).addClass("btn-help");
+            $(this).hide();
+
+        });
+
+        $("#cancelar").click(function(){
+            $('nuevaMateriaCursada').show();
+
+        });
+    });
+</script>
+@endsection
