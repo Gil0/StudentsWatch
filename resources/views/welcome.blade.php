a428686b303b1326182d7b43b3c952d66d5c592d
nancytap <nancylykens@gmail.com>
2017-11-09 03:07:16 -0600

    agregando slider a pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,16 +16,14 @@
 </div>  
 
 <!------ Slider ------------>
-<div class="slider">
-	      	<div class="slider-wrapper theme-default">
-	            <div id="slider" class="nivoSlider">
-	                <img src="../assets/img/bueno.png" alt="" />
-	                <img src="images/banner3.jpg" alt="" />
-	                <img src="images/banner2.jpg" alt="" />
-	                <img src="images/banner1.jpg" alt="" />
-	                <img src="images/banner5.jpg" alt="" />
-	            </div>
-	       </div>
+<div id ="contenedor">
+	      	<ul><li><img src="../assets/img/3s.jpg" alt="" /></li>
+                <li><img src="../assets/img/6.jpg" alt="" /></li>
+                <li><img src="../assets/img/xx.jpg" alt="" /></li>
+               
+	                
+           </ul>
+    </div>
          </div>
          
   <!------End Slider ------------>    
@@ -34,7 +32,7 @@
         @if (Auth::user()->is_profesor == true)                                                        
             @if (Auth::user()->is_tutor == true)                
             <div class="navar">
-                <ul class="nav nav-pills"> 
+                <ul class="nav nav-pills "> 
                     <li><a href="{{ url('/login') }}">Inicio</a></li>
                     <li><a href="{{ url('/login') }}">Mis alumnos</a></li>
                     <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
@@ -89,7 +87,7 @@
                     <li><a href="{{ url('/Admin/Materias') }}">Materias</a></li>
                  </ul>
             </div>
-            <div class="row intro">
+            <div class="row intro logindiv1">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="panel panel-default">
                        <center> <div class="panel-heading">Bienvenido  ... </div></center>
