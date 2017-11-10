1e0f54442aabf4cee3f7a7cc43e0a1b81fcde0d3
Gil0 <gilrafael47@gmail.com>
2017-11-10 01:12:58 -0600

    Funcion misComentarios de rol profesor

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -75,15 +75,18 @@ class ProfesorController extends Controller
         return redirect()->action('ProfesorController@miInformacion',['id'=>$ID]);
     }
 
-
-    public function profesores()
-    {
-    $profesores=DB::table('users')
-    ->join('users', 'users.id' , '=' ,'profesores.user_d')
-    ->select('users.name', 'profesores.email', 'profesores.idProfesor', 'profesores.descripcion')
-    ->where('is_profesor',1);
-    return view('/Usuario/profesores')->with('profesores',$profesores);
-
+    public function profesores(){
+        $profesores=DB::table('users')
+            ->join('users', 'users.id' , '=' ,'profesores.user_d')
+            ->select('users.name', 'profesores.email', 'profesores.idProfesor', 'profesores.descripcion')
+            ->where('is_profesor',1);
+        return view('/Usuario/profesores')->with('profesores',$profesores);
     }
-
+    
+    public function misComentarios(Request $request,$id){   
+        $ID=Crypt::decrypt($id);          
+        $informacionProfesor = DB::table('profesores')->where('user_id',$ID)->first();              
+        $comentarios = DB::table('comentarios')->select('*')->where('idProfesor',$informacionProfesor->idProfesor)->where('status',true)->get();        
+        return view('/Profesor/MisComentarios')->with('comentarios',$comentarios);
+    }
 }
