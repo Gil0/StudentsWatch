b45d916677af29bbf161b86a5b4d290d63f0fac2
Manolo <manolinsk8@hotmail.com>
2017-11-15 00:28:23 -0600

    creacion de vista mi mapa (mapa alumno)

---
+++ resources/views/Usuario/mapaAlumno.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.app')
+
+@section('content')
+<!--<meta name="csrf_token" content="{{ csrf_token() }}" /> <!Se necestia este metadato para poder hacer AJAX, se envia el csrf_token al server para validar que si existe la sesion -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <style>
+        @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
+        @import url('https://fonts.googleapis.com/css?family=Anton');
+    
+        .profesor{
+            color: #3385ff;
+            font-family: 'Anton', sans-serif;
+            letter-spacing: 2px;
+            font-size: 30px;
+        }
+
+    </style>
+
+    @if (Auth::user()->is_admin == true)
+        @if (Auth::user()->is_profesor == true)                                                        
+            @if (Auth::user()->is_tutor == true)                
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mis alumnos</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Información</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                 </ul>
+            </div>
+            
+            @else                
+                <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Informacion</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>                                                
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores/') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                  </ul>
+            </div>
+           
+            @endif                                        
+        @else            
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Alumnos</a></li>
+                    <li><a href="{{ url('/Admin/Profesores') }}">Profesores</a></li>
+                    <li><a href="{{ url('/login') }}">Comentarios</a></li>
+                 </ul>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Bienvenido</div>
+                            <div class="panel-body">
+                                Hola Administrador.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>                                                  
+        @endif                                        
+    @else
+        @if (Auth::user()->is_profesor == true)
+            @if (Auth::user()->is_tutor == true)                
+            <div class="navar">
+                <ul class="nav nav-pills"> 
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Alumnos</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Información</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>
+                 </ul>
+            </div>
+                                                       
+            @else            
+            <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/Profesor/Informacion/'.encrypt(Auth::user()->id)) }}">Mi Informacion</a></li>
+                    <li><a href="{{ url('/login') }}">Mis Comentarios</a></li>                                                
+                  </ul>
+            </div>
+                                          
+            @endif
+        @else            
+            <div class="navar">
+                <ul class="nav nav-pills">
+                    <li><a href="{{ url('/login') }}">Inicio</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Mapa Curricular</a></li>
+                    <li><a href="{{ url('/login') }}">Mi Progreso</a></li>
+                    <li><a href="{{ url('/Usuario/Profesores') }}">Ver Profesores</a></li>
+                    <li><a href="{{ url('/Usuario/Materias') }}">Ver Materias</a></li>
+                 </ul>
+            </div>
+                                                             
+        @endif                                        
+    @endif 
+
+           <div class="row">
+           <div class="col-sm-10 col-sm-offset-1">
+                <div align="center" >
+                    <p class="profesor">Materias</p>
+                </div>
+                <div class="panelesp">
+                    <div class="panel-body">
+                        <table class="table table-hover">
+                            <thread>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre</th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                            @foreach($materias as $materias)
+                                <tr>
+                                    <th scope="row">{{$materias->idMateria}}</th>
+                                    <th>{{$materias->nombre}} </th>
+                                </tr>
+                             @endforeach
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+
+@endsection
