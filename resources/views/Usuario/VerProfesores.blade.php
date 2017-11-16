a0bd1a85e83bb28d1f9cb5b10b5b97afdad9445f
Gil0 <gilrafael47@gmail.com>
2017-11-16 09:46:58 -0600

    Solucion vista profesor

--- resources/views/Usuario/VerProfesores.blade.php
+++ resources/views/Usuario/VerProfesores.blade.php
@@ -6,16 +6,8 @@
 <style>
     @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
     @import url('https://fonts.googleapis.com/css?family=Anton');
-    body{
-        padding: 0;
-        margin: 0;
-       
-    }
- 
-
 </style>
-
-
+<div class="container">
     @if (Auth::user()->is_admin == true)
         @if (Auth::user()->is_profesor == true)                                                        
             @if (Auth::user()->is_tutor == true)                
@@ -104,93 +96,94 @@
         @endif                                        
     @endif 
 
-      <div class="panel panel-info">
-            <div class="panel-heading labelmenu">Información Personal</div>
-                <div "panel-body">
-                <form id="editarInformacion" class="form-horizontal" >                                                           
-                <div class="form-group">
-                <br/>
-                    <label class="col-sm-2 control-label">Nombre</label>
-                    <div class="col-sm-10">
-                    <input type="text" readonly class="form-control" id="name" name="name" value="{{$profesores->name}}">
+    <div class="row">
+        <div class="col-12 col-md-12">
+            <div class="panel panel-info">
+                <div class="panel-heading labelmenu">Información Personal</div>
+                    <div class="panel-body">                                                     
+                        <form class="form-horizontal">   
+                            <div class="form-group">
+                                <br/>
+                                    <label class="col-sm-2 control-label">Nombre</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control" id="name" name="name" value="{{$profesores->name}}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Matricula</label>
+                                <div class="col-sm-10">
+                                <input type="text" class="form-control" id="matricula" name="matricula" value="{{$profesores->matricula}}" readonly>
+                                </div>
+                            </div>       
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Correo Electronico</label>
+                                <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" value="{{$profesores->email}}" readonly>
+                                </div>
+                            </div>   
+                        
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Cubiculo</label>
+                                <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" value="{{$profesores->cubiculo}}" readonly>
+                                </div>
+                            </div> 
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Descripcion</label>
+                                <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" value="{{$profesores->descripcion}}" readonly>
+                                </div>
+                            </div>  
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Hobbies</label>
+                                <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" value="{{$profesores->hobbies}}" readonly>
+                                </div>
+                            </div> 
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Calificación</label>
+                                <div class="col-sm-10">
+                                <input type="input" class="form-control" id="calificacion" name="calificacion" value="{{$profesores->calificacion}}" readonly>
+                                </div>
+                            </div>
+                        </form>                     
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Matricula</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="matricula" name="matricula" value="{{$profesores->matricula}}" readonly>
-                    </div>
-                </div>       
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Correo Electronico</label>
-                    <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" value="{{$profesores->email}}" readonly>
-                    </div>
-                </div>   
-                       
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Cubiculo</label>
-                    <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" value="{{$profesores->cubiculo}}" readonly>
-                    </div>
-                </div> 
-
-                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Descripcion</label>
-                    <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" value="{{$profesores->descripcion}}" readonly>
-                    </div>
-                </div>  
-
-                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Hobbies</label>
-                    <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" value="{{$profesores->hobbies}}" readonly>
-                    </div>
-                </div> 
-
-                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Calificación</label>
-                    <div class="col-sm-10">
-                    <input type="input" class="form-control" id="calificacion" name="calificacion" value="{{$profesores->calificacion}}" readonly>
-                    </div>
-                </div>
-
-                </form>        
             </div>
-      </div>
-      
-
-            <!--Informacion Formacion Academica-->
+        </div>
+        <!--Informacion Formacion Academica-->
             <div class="panel panel-info">
                 <div class="panel-heading">Informacion Academica</div>                        
                     @if(!empty($formacion_academica))
                     <div class="panel-body">   
-                    <table class="table table-hover">
-                            <thread>
-                                <tr>                                    
-                                    <th>Escuela</th>
-                                    <th>Estudios</th>
-                                    <th>Periodo</th>                                    
+                        <table class="table table-hover">
+                                <thread>
+                                    <tr>                                    
+                                        <th>Escuela</th>
+                                        <th>Estudios</th>
+                                        <th>Periodo</th>                                    
+                                    </tr>
+                                </thread>                     
+                            @foreach($formacion_academica as $formacion_academica)                                                                            
+                            <tbody>
+                                <tr>
+                                    <th>{{$formacion_academica->escuela}}</th>
+                                    <th>{{$formacion_academica->estudios}}</th>
+                                    <th>{{$formacion_academica->periodo}}</th>
                                 </tr>
-                            </thread>                     
-                        @foreach($formacion_academica as $formacion_academica)                                                                            
-                        <tbody>
-                            <tr>
-                                <th>{{$formacion_academica->escuela}}</th>
-                                <th>{{$formacion_academica->estudios}}</th>
-                                <th>{{$formacion_academica->periodo}}</th>
-                            </tr>
-                        </tbody>                                                                                                                                                                                
-                        @endforeach
-                    </table>   
+                            </tbody>                                                                                                                                                                                
+                            @endforeach
+                        </table>   
                     </div>                                            
                     @endif    
-             </div>                         
-            </div>
-            <!--Informacion Informacion Laboral-->
+            </div>                         
+            
+             <!--Informacion Informacion Laboral-->
             <div class="panel panel-info">
                 <div class="panel-heading">Informacion Laboral</div>
                     @if(!empty($informacion_laboral))
@@ -213,14 +206,13 @@
                         </tbody>                                              
                         @endforeach
                         </table>
-                        </div>
+                    </div>
                     @endif         
             </div>
             
-            </div>
-        </div>
-    </div>
-</div>
+           
+    
 
+</div>
 @endsection 
 
