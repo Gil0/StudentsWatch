56422f8271a7c59094f071f492a630ec8e097506
leonelvazquezz <leonelvazquezz@hotmail.com>
2017-11-15 23:32:31 -0600

    header responsive (falta cambiar colores)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-
-
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>StudentsWatch</title>
 
     <!-- Font Awesome -->    
@@ -16,7 +12,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
+    
     <!--Mi CSS-->
     <link rel="stylesheet" href="../assets/css/estilos.css" >
 
@@ -31,23 +27,18 @@
     <style>
         body {
             font-family: 'Lato';
-           /*background-color:#AED6F1; */
-          /*<img src="../assets/img/book.jpg">*/
-        
         }
 
         .fa-btn {
             margin-right: 6px;
         }
-
-       
     </style>
 </head>
-<!--<body background ="../assets/img/hu.jpg" id="app-layout">-->
 <body id="app-layout">
-    <nav class=" header header-top">
+    <nav class="navbar navbar-default navbar-static-top">
+    <nav class=" header header-top"> 
         <div class="container">
-            <div class="navbar-header  ">
+            <div class="navbar-header">
 
                 <!-- Collapsed Hamburger -->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
@@ -57,12 +48,13 @@
                     <span class="icon-bar"></span>
                 </button>
 
+
+
+
+
                 <!-- Branding Image -->
-                <a class="navbar-brand " href="{{ url('/') }}">
-                   <div class="letra"> StudentsWatch</div>
-                <!--<img  class="pequeña" style="float:left;"  src="../assets/img/logo.png">-->
-              
-                
+                <a class="navbar-brand" href="{{ url('/') }}">
+                <div class="letra"> StudentsWatch</div>
                 </a>
             </div>
 
@@ -113,6 +105,7 @@
                 </ul>
             </div>
         </div>
+    </div>
     </nav>
 
     @yield('content')
@@ -121,8 +114,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>    
-  <!--FOOTER-->
-  <footer style="padding-bottom: 0;">
+   <!--FOOTER-->
+   <footer style="padding-bottom: 0;">
   <div class="container">
       <div class="row ">
           <div class="col-xs-12 col-sm-6 ">
